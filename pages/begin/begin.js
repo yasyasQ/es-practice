2dd5f462d7d854a8b88100716fa72b5cc9313081
yasyasQ <601763224@qq.com>
2022-05-07 22:37:41 +0800

    5.7修改时间选择器和轮播图组件

--- pages/begin/begin.js
+++ pages/begin/begin.js
@@ -1,37 +1,31 @@
 // pages/begin/begin.js
+// let app = getApp()
 
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    array:[],
-    // 轮播图配置
-    autoplay:true,
-    interval:3000,
-    duration:1200,
+    // 轮播图配置 
     // paymentIndex:0,
     // index:0,
     // date:"",
-    // goal_info:""
+    // goal_info:""    
+    lunboData:[],
+
+    // 日期选择器
+    date:'',
+
+    usergoal:"",
+    usernow:"",
+    usertoday:"",
+
   },
-//   onLoad: function (options) {
-//     //获取当前时间
-//     var DATE = util.formDate(new Date());
-//     this.setData({
-//     date: DATE,
-// });
-//   },
-//   bindDateChange: function (e) {
-//     console.log( e.detail.value)
-//     this.setData({
-//       date: e.detail.value
-//     })
-//   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+  // 轮播图
   onLoad: function () {
     var that = this;
     var data = {
@@ -53,5 +47,33 @@ Page({
   that.setData({
     lunboData: data.datas
   })
+  },
+  // 时间选择器
+  changeDate(e){
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  //记录输入的数据
+  getUsergoal:function(usergoal){ 
+    console.log(usergoal.detail.value)
+    this.setData({ 
+      usergoal: usergoal.detail.value
+    })
+  },
+
+  getUsernow:function(usernow){ 
+    console.log(usernow.detail.value)
+    this.setData({ 
+      usernow: usernow.detail.value
+    })
+  },
+
+  getUsertoday:function(usertoday){ 
+      console.log(usertoday.detail.value)
+    this.setData({ 
+      usertoday: usertoday.detail.value
+    }) 
   }
+
 })
